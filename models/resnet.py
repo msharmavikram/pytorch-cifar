451dc05fc48393f65c28836d27f2cef067ed525b
Vikram Sharma <vsm2@illinois.edu>
2019-06-07 09:39:46 -0700

    Update resnet.py
    Modifications to the https://gist.github.com/sublee/55ed4181e20dd59690188b279465d705 implementation to integrate it in pytorch-cifar codebase

--- models/resnet.py
+++ models/resnet.py
@@ -1,121 +1,98 @@
-'''ResNet in PyTorch.
+"""A ResNet implementation but using :class:`nn.Sequential`. :func:`resnet101`
+returns a :class:`nn.Sequential` instead of ``ResNet``.
+This code is transformed :mod:`torchvision.models.resnet`.
+"""
+from collections import OrderedDict
+from typing import Any, List
 
-For Pre-activation ResNet, see 'preact_resnet.py'.
-
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-'''
-import torch
+from torch import Tensor
 import torch.nn as nn
-import torch.nn.functional as F
+
+from resnet.bottleneck import bottleneck
+from resnet.flatten import flatten
+
+__all__ = ['ResNet101']
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
+class Flatten(nn.Module):
+    """Flattens any input tensor into an 1-d tensor."""
 
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+    def forward(self, x: Tensor):  # type: ignore
+        return x.view(x.size(0), -1)
 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+
+def build_resnet(layers: List[int],
+                 num_classes: int = 10, #1000--> 10 for cifar10
+                 ) -> nn.Sequential:
+    """Builds a ResNet as a simple sequential model.
+    Note:
+        The implementation is copied from :mod:`torchvision.models.resnet`.
+    """
+    inplanes = 64
+
+    def make_layer(planes: int, blocks: int, stride: int = 1) -> nn.Sequential:
+        nonlocal inplanes
+
+        downsample = None
+        if stride != 1 or inplanes != planes * 4:
+            downsample = nn.Sequential(
+                nn.Conv2d(inplanes, planes * 4,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * 4),
             )
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+        layers.append(bottleneck(inplanes, planes, stride, downsample))
+        inplanes = planes * 4
+        for _ in range(1, blocks):
+            layers.append(bottleneck(inplanes, planes))
+
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
+    # Build ResNet as a sequential model.
+    model = nn.Sequential(OrderedDict([
+        ('conv1', nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)),
+        ('bn1', nn.BatchNorm2d(64)),
+        ('relu', nn.ReLU()),
+        ('maxpool', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+
+        ('layer1', make_layer(64, layers[0])),
+        ('layer2', make_layer(128, layers[1], stride=2)),
+        ('layer3', make_layer(256, layers[2], stride=2)),
+        ('layer4', make_layer(512, layers[3], stride=2)),
+
+        ('avgpool', nn.AdaptiveAvgPool2d((1, 1))),
+        ('flat', Flatten()),
+        ('fc', nn.Linear(512 * 4, num_classes)),
+    ]))
+
+    # Flatten nested sequentials.
+    model = flatten(model)
+
+    # Initialize weights for Conv2d and BatchNorm2d layers.
+    def init_weight(m: nn.Module) -> None:
+        if isinstance(m, nn.Conv2d):
+            assert isinstance(m.kernel_size, tuple)
+            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+
+            m.weight.requires_grad = False
+            m.weight.normal_(0, 2. / n**0.5)
+            m.weight.requires_grad = True
+
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.requires_grad = False
+            m.weight.fill_(1)
+            m.weight.requires_grad = True
+
+            m.bias.requires_grad = False
+            m.bias.zero_()
+            m.bias.requires_grad = True
+
+    model.apply(init_weight)
+
+    return model
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2,2,2,2])
-
-def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,3])
-
-def ResNet50():
-    return ResNet(Bottleneck, [3,4,6,3])
-
-def ResNet101():
-    return ResNet(Bottleneck, [3,4,23,3])
-
-def ResNet152():
-    return ResNet(Bottleneck, [3,8,36,3])
-
-
-def test():
-    net = ResNet18()
-    y = net(torch.randn(1,3,32,32))
-    print(y.size())
-
-# test()
+def ResNet101() -> nn.Sequential:
+    """Constructs a ResNet-101 model."""
+    return build_resnet([3, 4, 23, 3], 10)
